90aaa99816b0bfb93468559439ef14ff9acb6c1c
Sheerin Naseem <sheerin.naseem@gmail.com>
2018-09-26 17:07:34 +0530

    Menu, Login, onboarding and video listing page added

---
+++ Assets/Script/orientation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class orientation : MonoBehaviour {
+
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+
+    
+}
