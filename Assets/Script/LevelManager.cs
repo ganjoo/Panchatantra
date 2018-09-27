a0a484209bb8b1696fb362085ead0ff8ce4251e8
Sheerin Naseem <sheerin.naseem@gmail.com>
2018-09-27 17:20:41 +0530

    Onboarding screen using google material design plugin

---
+++ Assets/Script/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for " + name);
+#pragma warning disable CS0618 // Type or member is obsolete
+        Application.LoadLevel(name);
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
+}
