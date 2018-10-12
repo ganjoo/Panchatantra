26985a89fbcc48a776bf888e263c2458ae5be1aa
Sheerin Naseem <sheerin.naseem@gmail.com>
2018-10-12 18:47:42 +0530

    Added Drag Script and Sample Scne for Drag

---
+++ Assets/Script/Dragobject.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragobject : MonoBehaviour {
+  private Vector3 screenPoint;
+  private Vector3 offset;
+  public GameObject target;
+  private Vector3 initialPos;
+
+  void OnMouseDown(){
+    screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+    offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+   
+
+  }
+
+
+  void OnMouseUp(){
+
+     float distance = Vector2.Distance(target.transform.position,transform.position);
+    Debug.Log(distance);
+    if(distance < 2){
+      transform.position = target.transform.position;
+    }
+    else{
+      transform.position = initialPos;
+    }
+    Debug.Log("Mouse Up");
+  }
+    
+  void OnMouseDrag(){
+    Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+    Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+    transform.position = cursorPosition;
+        Debug.Log("Mouse Dragged");
+
+  }
+	// Use this for initialization
+	void Start () {
+		initialPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
